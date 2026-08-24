028bd38dd38ef544e339fe0a0d0899c0432a68a0
Ansh Jindal <jndl.ansh@gmail.com>
2026-08-24 09:34:12 +0530

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool alphaNum(char c){
+        return ((c>='a' && c<='z') || (c>='A' && c<='Z') || (c>='0' && c<='9'));
+    }
+    bool isPalindrome(string s) {
+        int n = s.size();
+        int l=0, r=n-1;
+
+        while(l<r){
+            while(l<r && !alphaNum(s[l])) l++;
+
+            while(l<r && !alphaNum(s[r])) r--;
+
+            if(tolower(s[l])!=tolower(s[r])) return false;
+
+            l++;
+            r--;
+        }
+        return true;
+    }
+};
