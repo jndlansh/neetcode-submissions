3f5b4686233d21c70bbc2943f91d0a4b0be6a5d8
Ansh Jindal <jndl.ansh@gmail.com>
2026-08-28 15:40:30 +0530

    Add: minimum-window-with-characters - submission-2

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-2.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    string minWindow(string s, string t) {
+        int n = s.size();
+        int m = t.size();
+
+        if(m>n) return "";
+
+        vector<int>mp(128, 0);
+
+        for(auto it : t){
+            mp[it]++;
+        }
+
+        int start=0, l=0, minLen=INT_MAX, cnt=m;
+
+        for(int r=0; r<n; r++){
+            if(mp[s[r]]>0) cnt--;
+            mp[s[r]]--;
+
+            while(cnt == 0){
+                if(minLen>r-l+1){
+                    minLen = r-l+1;
+                    start = l;
+                }
+                mp[s[l]]++;
+                if(mp[s[l]]>0) cnt++;
+                l++;
+            }
+        }
+        return minLen == INT_MAX ? "" : s.substr(start, minLen);
+    }
+};
