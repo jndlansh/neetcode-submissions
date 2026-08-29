8b4ac130a2ebf75a544f448bb7917337404b21d5
Ansh Jindal <jndl.ansh@gmail.com>
2026-08-29 10:58:39 +0530

    Add: anagram-groups - submission-4

---
+++ Data Structures & Algorithms/anagram-groups/submission-4.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>>mp;
+        for(auto s : strs){
+            vector<int>hash(26, 0);
+            for(auto it : s){
+                hash[it-'a']++;
+            }
+            string key = "";
+            for(int i=0; i<26; i++){
+                key += to_string(hash[i])+'#';
+            }
+            mp[key].push_back(s);
+        }
+        vector<vector<string>>ans;
+        for(auto it : mp){
+            ans.push_back(it.second);
+        }
+        return ans;
+    }
+};
