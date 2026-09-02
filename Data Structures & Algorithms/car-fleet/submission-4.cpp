55d5ef9df9483ecc729c3fe3a8e5bde028e8814c
Ansh Jindal <jndl.ansh@gmail.com>
2026-09-03 00:28:45 +0530

    Add: car-fleet - submission-4

---
+++ Data Structures & Algorithms/car-fleet/submission-4.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int carFleet(int target, vector<int>& pos, vector<int>& speed) {
+        int n = pos.size();
+        vector<pair<int, int>>cars(n);
+        stack<double>st;
+
+        for(int i=0; i<n; i++){
+            cars[i]=make_pair(pos[i], speed[i]);
+        }
+
+        sort(cars.rbegin(), cars.rend());
+
+        for(int i=0; i<n; i++){
+            double time = (double)(target - cars[i].first)/(double)(cars[i].second);
+            if(st.empty() || st.top()<time){
+                st.push(time);
+            }
+        }
+        return st.size();
+    }
+};
