c7f56da2278bd074d70d8f0e84c5f5bf34904c19
Ansh Jindal <jndl.ansh@gmail.com>
2026-09-12 00:40:18 +0530

    Add: task-scheduling - submission-0

---
+++ Data Structures & Algorithms/task-scheduling/submission-0.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int leastInterval(vector<char>& tasks, int n) {
+        vector<int>freq(26, 0);
+        for(auto it : tasks){
+            freq[it-'A']++;
+        }
+        priority_queue<int>pq;
+        for(int f : freq){
+            if(f>0){
+                pq.push(f);
+            }
+        }
+
+        int time = 0;
+        queue<pair<int, int>>q;
+        while(!pq.empty() || !q.empty()){
+            time++;
+
+            if(pq.empty()){
+                time = q.front().second;
+            }else{
+                int cnt = pq.top() - 1;
+                pq.pop();
+                if(cnt>0){
+                    q.push({cnt, time+n});
+                }
+            }
+
+            if(!q.empty() && q.front().second == time){
+                pq.push(q.front().first);
+                q.pop();
+            }
+        }
+        return time;
+    }
+};
