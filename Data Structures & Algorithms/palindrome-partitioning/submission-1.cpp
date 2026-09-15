ccc0eba61d1308a90d3a16cb19f4f591d4531ba8
Ansh Jindal <jndl.ansh@gmail.com>
2026-09-15 11:19:23 +0530

    Add: palindrome-partitioning - submission-1

---
+++ Data Structures & Algorithms/palindrome-partitioning/submission-1.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<vector<string>>ans;
+
+    bool isPalin(string s, int l, int r){
+        while(l<r){
+            if(s[l] != s[r])
+                return false;
+            l++;
+            r--;
+        }
+        return true;
+    }
+
+    void solve(string s, vector<string>& temp, int start){
+        if(start >= s.size()){
+            ans.push_back(temp);
+            return;
+        }
+        for(int i=start; i<s.size(); i++){
+            if(!isPalin(s, start, i)) continue;
+            temp.push_back(s.substr(start, i-start+1));
+
+            solve(s, temp, i+1);
+
+            temp.pop_back();
+        }
+    }
+    vector<vector<string>> partition(string s) {
+        vector<string>temp;
+        solve(s, temp, 0);
+        return ans;
+    }
+};
