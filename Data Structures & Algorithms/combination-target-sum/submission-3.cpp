dc1f01e655ce2ef2eaf0ce23e4be22bc38394e46
Ansh Jindal <jndl.ansh@gmail.com>
2026-09-17 10:04:21 +0530

    Add: combination-target-sum - submission-3

---
+++ Data Structures & Algorithms/combination-target-sum/submission-3.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<vector<int>>ans;
+    void solve(vector<int>& nums, vector<int>& temp, int target, int i) {
+        if(target == 0){
+            ans.push_back(temp);
+            return;
+        }
+        if(i>=nums.size() || target<0) return;
+        temp.push_back(nums[i]);
+        solve(nums, temp, target-nums[i], i);
+        temp.pop_back();
+        solve(nums, temp, target, i+1);
+    }
+    vector<vector<int>> combinationSum(vector<int>& nums, int target) {
+        vector<int>temp;
+        solve(nums, temp, target, 0);
+        return ans;
+    }
+};
