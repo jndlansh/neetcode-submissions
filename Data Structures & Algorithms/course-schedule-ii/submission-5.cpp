f135d5e926bf568a47ab21a7cb78fe9b84392281
Ansh Jindal <jndl.ansh@gmail.com>
2026-09-25 11:02:23 +0530

    Add: course-schedule-ii - submission-5

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-5.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    vector<int> findOrder(int V, vector<vector<int>>& prerequisites) {
+        vector<vector<int>>adj(V);
+        for(auto it : prerequisites) {
+            adj[it[1]].push_back(it[0]);
+        }
+
+        vector<int>indegree(V, 0);
+        for(int u=0; u<V; u++) {
+            for(auto v : adj[u]) {
+                indegree[v]++;
+            }
+        }
+
+        queue<int>q;
+        for(int i=0; i<V; i++) {
+            if(indegree[i] == 0) {
+                q.push(i);
+            }
+        }
+
+        vector<int>ans;
+        while(!q.empty()) {
+            int node = q.front();
+            q.pop();
+            ans.push_back(node);
+            for(auto nei : adj[node]) {
+                indegree[nei]--;
+                if(indegree[nei] == 0){
+                    q.push(nei);
+                }
+            }
+        }
+        if(ans.size() != V) return {};
+        return ans;
+    }
+};
